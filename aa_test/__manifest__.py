f23432ff23659e2f45aa3f2bcf45b65364113bf2
Yenthe Van Ginneken <yenthevg@gmail.com>
2022-06-06 11:08:32 +0200

    [BMP] aa_test: bump app version

--- aa_test/__manifest__.py
+++ aa_test/__manifest__.py
@@ -17,7 +17,7 @@
     # Check https://github.com/odoo/odoo/blob/13.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
     'category': 'Uncategorized',
-    'version': '0.1',
+    'version': '0.2',
     'price': 14.95,
     'currency': 'EUR',
 
